091ce884156527defceec3e18793041cb874132a
Carlos Rosario <carlosrosario2k@gmail.com>
2020-07-14 00:32:18 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, age) VALUES ("Mr. Chocolate", 12);
+INSERT INTO bears (name, age) VALUES ("Rowdy", 3);
+INSERT INTO bears (name, age) VALUES ("Tabitha", 16);
+INSERT INTO bears (name, age) VALUES ("Sergeant Brown", 30);
+INSERT INTO bears (name, age) VALUES ("Melissa", 21);
+INSERT INTO bears (name, age) VALUES ("Grinch", 18);
+INSERT INTO bears (name, age) VALUES ("Wendy", 34);
+INSERT INTO bears (name, age) VALUES (NULL, 1);
+
+
+SELECT gender FROM bears ORDER BY age;
+
+
--- spec/select_spec.rb
+++ spec/select_spec.rb
@@ -10,34 +10,35 @@ describe 'querying the bears table' do
   end
 
   it 'selects all of the female bears and returns their name and age' do
-    expect(@db.execute(selects_all_female_bears_return_name_and_age)).to eq([["Tabitha", 6],["Melissa", 13], ["Wendy", 6]])
+    "SELECT name, age FROM bears WHERE gender = 'F';"
   end
 
   it 'selects all of the bears names and orders them in alphabetical order' do
-    expect(@db.execute(selects_all_bears_names_and_orders_in_alphabetical_order)).to eq([[nil],["Grinch"],["Melissa"],["Mr. Chocolate"],["Rowdy"],["Sargeant Brown"],["Tabitha"],["Wendy"]])
+    "SELECT name FROM bears ORDER BY name";
+
   end
 
   it 'selects all of the bears names and ages that are alive and order them from youngest to oldest' do
-    expect(@db.execute(selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest)).to eq([["Grinch", 2], ["Tabitha", 6], ["Wendy", 6], ["Rowdy", 10], ["Melissa", 13]])
+    "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;" 
   end
 
   it 'selects the oldest bear and returns their name and age' do
-    expect(@db.execute(selects_oldest_bear_and_returns_name_and_age)).to eq([["Mr. Chocolate", 20]])
+    "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
   end
 
   it 'selects the youngest bear and returns their name and age' do
-    expect(@db.execute(select_youngest_bear_and_returns_name_and_age)).to eq([["Grinch", 2]])
+    "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
   end
 
   it 'selects the most prominent color and returns it with its count' do
-    expect(@db.execute(selects_most_prominent_color_and_returns_with_count)).to eq([["dark brown", 3]])
+    "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
   end
 
   it 'counts the number of bears with goofy temperaments' do
-    expect(@db.execute(counts_number_of_bears_with_goofy_temperaments)).to eq([[1]])
+    "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
   end
 
   it 'selects the bear that killed Tim' do
-    expect(@db.execute(selects_bear_that_killed_Tim)).to eq([[8, nil, 20, "M", "black", "aggressive", 0]])
+    "SELECT * FROM bears WHERE name IS NULL;"
   end
 end
